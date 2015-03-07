02393a1d879aa91e1453f514b4c79551322a73ef
Chris Donahue <cdonahue@famigo.com>
2015-03-06 16:47:23 -0800

    still working

--- Source/main.cpp
+++ Source/main.cpp
@@ -45,28 +45,34 @@ int main (int argc, char* argv[]) {
 	voice_1.table_set(sine_table_length, sine_table.channel_pointer_read(values::channel_zero));
 
 	// test voice
-	voice_1.i_set(static_cast<types::sample>(1.0f));
-	voice_1.f_m_set(static_cast<types::sample>(100.0f));
-	voice_1.f_c_set(static_cast<types::sample>(440.0f));
-	voice_1.a_set(static_cast<types::sample>(0.67f));
+	voice_1.f_m_set(static_cast<types::sample>(99.7719/target_sample_rate));
+	voice_1.i_set(static_cast<types::sample>(1.65366));
+	voice_1.f_c_set(static_cast<types::sample>(438.909/target_sample_rate));
+	voice_1.a_set(static_cast<types::sample>(-0.55803));
 	voice_1.prepare(target_sample_rate, target_length);
 	voice_1.perform(candidate_buffer, target_length, 0, 0);
 	candidate_buffer.resize(1, target_length);
-	helpers::io::wav_file_save("voice_1_test.wav", target_sample_rate, 32, candidate_buffer);
+	helpers::io::wav_file_save("voice_1_test_new2.wav", target_sample_rate, 32, candidate_buffer);
 	candidate_buffer.resize(2, target_length);
 
 	// define fitness function
-	FitFunc fm_simple_error_amp = [&voice_1, &candidate_buffer, &target_buffer](const double *x, const int N)
+	FitFunc fm_simple_error_amp = [&voice_1, &candidate_buffer, target_sample_rate, &target_buffer](const double *x, const int N)
 	{
 		// set parameters
-		voice_1.i_set(static_cast<types::sample>(x[0]));
-		voice_1.f_m_set(static_cast<types::sample>(x[1]));
-		voice_1.f_c_set(static_cast<types::sample>(x[2]));
+		voice_1.f_m_set(static_cast<types::sample>(x[0]/target_sample_rate));
+		voice_1.i_set(static_cast<types::sample>(x[1]));
+		voice_1.f_c_set(static_cast<types::sample>(x[2]/target_sample_rate));
 		voice_1.a_set(static_cast<types::sample>(x[3]));
+
+		std::cout << "{" << x[0] << ", "
+						<< x[1] << ", "
+						<< x[2] << ", "
+						<< x[3] << "}" << std::endl;
 
 		types::index target_length = candidate_buffer.channel_buffer_length_get();
 
 		// perform
+		voice_1.reset();
 		voice_1.perform(candidate_buffer, target_length, 0, 0);
 
 		// return fitness function result
@@ -90,7 +96,7 @@ int main (int argc, char* argv[]) {
 
 	// define solution bounds
 	double lbounds[] = {0.0, 0.0, 0.0, 0.0};
-	double ubounds[] = {22050.0, 10.0, 44100.0, 1.0};
+	double ubounds[] = {target_sample_rate, 10.0, target_sample_rate, 1.0};
 	GenoPheno<pwqBoundStrategy> gp(lbounds, ubounds, dim);
 
 	// define solution initial values
